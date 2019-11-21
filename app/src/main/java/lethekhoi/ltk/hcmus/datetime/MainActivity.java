b97bf3a4e1481ee14efbc6ad0cf3cb85522e3dd0
lethekhoi <lethekhoi@gmail.com>
2019-11-21 21:12:10 +0700

    init

---
+++ app/src/main/java/lethekhoi/ltk/hcmus/datetime/MainActivity.java
@@ -0,0 +1,20 @@
+package lethekhoi.ltk.hcmus.datetime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
